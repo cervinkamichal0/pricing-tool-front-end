b3e489cd0696426117bea70bd5143499450d98e2
cervinkamichal0 <cervinkamichal0@gmail.com>
2025-03-03 21:14:52 +0100

    created layout

---
+++ PricingTool Front end/Models/UserAdd.cs
@@ -0,0 +1,8 @@
+﻿namespace PricingTool_Front_end.Models;
+
+public class UserAdd
+{
+    public string Title { get; set; }
+
+    public string Descritpiton { get; set; }
+}
---
+++ PricingTool Front end/Pages/Home.razor.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Components;
+using System.Diagnostics.CodeAnalysis;
+using System.Net.Http.Json;
+using System.Xml.Linq;
+
+namespace PricingTool_Front_end.Pages
+{
+    partial class Home
+    {
+        [Inject]
+        [NotNull]
+        HttpClient Http { get; set; }
+        private string? Title { get; set; }
+
+        private string? Description { get; set; }
+
+        private async Task FetchSimilarAds()
+        {
+            HttpClient client = new HttpClient();
+
+            var data = new Dictionary<string, string>
+            {
+                { "title", Title },
+                { "description", Description }
+            };
+
+            var response = await Http.PostAsJsonAsync("/similar_ads", data);
+
+
+        }
+    }
+}
