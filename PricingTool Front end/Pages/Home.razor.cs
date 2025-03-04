2d1c753f29c6c59e8799913777bf18292f220cf6
Michal Červinka <cervinkamichal0@gmail.com>
2025-03-04 15:27:18 +0100

    working form

--- PricingTool Front end/Pages/Home.razor.cs
+++ PricingTool Front end/Pages/Home.razor.cs
@@ -2,31 +2,26 @@
 using System.Diagnostics.CodeAnalysis;
 using System.Net.Http.Json;
 using System.Xml.Linq;
+using PricingTool_Front_end.Models;
 
 namespace PricingTool_Front_end.Pages
 {
     partial class Home
     {
-        [Inject]
-        [NotNull]
-        HttpClient Http { get; set; }
-        private string? Title { get; set; }
-
-        private string? Description { get; set; }
+        [Inject] [NotNull] HttpClient? Http { get; set; }
+        private UserAdd UsersAdd { get; set; } = new();
 
         private async Task FetchSimilarAds()
         {
-            HttpClient client = new HttpClient();
-
             var data = new Dictionary<string, string>
             {
-                { "title", Title },
-                { "description", Description }
+                { "title", UsersAdd.Title },
+                { "description", UsersAdd.Descritpiton }
             };
 
             var response = await Http.PostAsJsonAsync("/similar_ads", data);
-
-
+            
+            Console.WriteLine((response));
         }
     }
 }
