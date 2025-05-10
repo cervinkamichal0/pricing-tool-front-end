2cc4a67271ed6ff8477d08b8be2fbe89f209e860
cervinkamichal0 <cervinkamichal0@gmail.com>
2025-05-10 16:00:10 +0200

    Using seperate component to didplay similar ads

---
+++ PricingTool Front end/Components/AdsDisplay.razor.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Components;
+using System.Diagnostics.CodeAnalysis;
+
+namespace PricingTool_Front_end.Components;
+
+public partial class AdsDisplay
+{
+    [Parameter, EditorRequired, NotNull]
+    public SimilarAdsResponse? AdsResponse { get; set; }
+}
